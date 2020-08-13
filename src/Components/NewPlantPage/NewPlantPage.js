9e10fb38a7e4b6bfedc459c663a57f9f153b4626
Aneta Szajowska <an.szajowska@gmail.com>
2020-08-13 12:58:41 +0200

    Add moment to dependencies.

--- src/Components/NewPlantPage/NewPlantPage.js
+++ src/Components/NewPlantPage/NewPlantPage.js
@@ -15,10 +15,13 @@ class NewPlantPage extends React.Component{
         name: '',
         wateringInterval: '',
         lastWatering: '',
+        nextWatering: '',
         spraingInterval: '',
         lastSpraing: '',
+        nextSpraing: '',
         feedingInterval: '',
         lastFeeding: '',
+        nextFeeding: '',
         place: '',
         notes: ''
     })
@@ -32,6 +35,29 @@ class NewPlantPage extends React.Component{
         })
     }
 
+    handleSelectChange = (event) => {
+        this.setState({
+            plant: {
+                ...this.state.plant,
+            wateringInterval: event.target.value
+            }
+        })
+    }
+
+    // handleSubmit = (event) => {
+    //     event.preventDefault();
+    //
+    //     // const plant = this.state.plant;
+    //     //
+    //     // axios.post('',
+    //     //     {...plant},{ headers: { 'Content-Type': 'application/json' } } )
+    //     //     .then(res => {
+    //     //         console.log(res);
+    //     //         console.log(plant);
+    //     //     })
+    // }
+
+
     render(){
         const { plant } = this.state;
 
@@ -39,31 +65,32 @@ class NewPlantPage extends React.Component{
             <div className="newPlantFormPage">
                 <div>
                     <h1>NEW PLANT FORM</h1>
-                    <div className="newPlantForm">
+                    <form className="newPlantForm" onSubmit={this.handleSubmit}>
                         <label>Name: </label>
                         <input type="text" name="name" value={plant.name} onChange={this.handleInput}/>
                         <label>Watering interval:</label>
-                        <select value={plant.wateringInterval} onChange={this.handleInput}>
+                        <select value={plant.wateringInterval} onChange={this.handleSelectChange}>
                             <option value="everyDay">Every day</option>
                             <option value="onceAWeek">Once a week</option>
                             <option value="twicePerWeek">Twice a week</option>
                             <option value="threeTimesAWeek">Three times a week</option>
                             <option value="onceAMonth">Once a month</option>
                         </select>
+                        <p>{this.state.nextWatering}</p>
                         <label>Last watering:</label>
                         <input type="date" name="lastWatering" value={plant.lastWatering} onChange={this.handleInput}/>
                         <label>Spraing interval:</label>
-                        <select value={plant.spraingInterval} onChange={this.handleInput}>
+                        <select value={plant.spraingInterval} onChange={this.handleSelectChange}>
                             <option value="everyDay">Every day</option>
                             <option value="onceAWeek">Once a week</option>
-                            <option value="twicePerWeek">Twice a week</option>
+                            <option value="twiceAWeek">Twice a week</option>
                             <option value="threeTimesAWeek">Three times a week</option>
                             <option value="onceAMonth">Once a month</option>
                         </select>
                         <label>Last spraing:</label>
                         <input type="date" name="lastSpraing" value={plant.lastSpraing} onChange={this.handleInput}/>
                         <label>Feeding interval:</label>
-                        <select value={plant.feedingInterval} onChange={this.handleInput}>
+                        <select value={plant.feedingInterval} onChange={this.handleSelectChange}>
                             <option value="everyDay">Every day</option>
                             <option value="onceAWeek">Once a week</option>
                             <option value="twicePerWeek">Twice a week</option>
@@ -76,9 +103,9 @@ class NewPlantPage extends React.Component{
                         <input type="text" name="place" value={plant.place} onChange={this.handleInput}/>
                         <label>Notes:</label>
                         <textarea name="notes" value={plant.notes} onChange={this.handleInput}/>
-                        <button type="submit" className="submitButton">SUBMIT</button>
+                        <input type="submit" value="SUBMIT" className="submit-button" />
                         <GoHomeButton />
-                    </div>
+                    </form>
                     {console.log(plant)}
                 </div>
             </div>
