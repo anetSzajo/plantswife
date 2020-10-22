92631c14ca5d133089311b76065444a1f4df9320
Aneta Szajowska <an.szajowska@gmail.com>
2020-10-22 11:48:03 +0200

    Updates

---
+++ precache-manifest.e12e1b95668971228824f9b1f8d126fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a9f29caa1838a66bff631e5a6198fb",
+    "url": "/plantswife/index.html"
+  },
+  {
+    "revision": "7b8e455dffdad7fd8ac9",
+    "url": "/plantswife/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "e48e51d39c91227a1211",
+    "url": "/plantswife/static/css/main.22e7f554.chunk.css"
+  },
+  {
+    "revision": "7b8e455dffdad7fd8ac9",
+    "url": "/plantswife/static/js/2.50f84346.chunk.js"
+  },
+  {
+    "revision": "e8d03058ef75c34ec64ac1b9d995ff25",
+    "url": "/plantswife/static/js/2.50f84346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48e51d39c91227a1211",
+    "url": "/plantswife/static/js/main.520c3d5d.chunk.js"
+  },
+  {
+    "revision": "505c634717c8ec21952d",
+    "url": "/plantswife/static/js/runtime-main.c23a9043.js"
+  }
+]);
