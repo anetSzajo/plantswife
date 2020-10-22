a15ffcbf352bdf9e65369d5be2c1d6740ddb4a71
Aneta Szajowska <an.szajowska@gmail.com>
2020-10-22 13:13:15 +0200

    Revert github pages

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-
-describe('asd', function () {
-
-});
-
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
